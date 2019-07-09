698efcccc6d7dae3346ecabca96a3f4fbc81984e
2019-Q2_SOW_DEV_033 <2019-Q2_SOW_DEV_033@gmail.com>
2019-07-09 19:30:03 +0200

    Prepared Spring Boot Application From Maven Project using Spring Boot Dependencies and a Starter Class

---
+++ bowling-game/src/test/java/com/exam/kata/SpringBootWebApplicationTest.java
@@ -0,0 +1,13 @@
+package com.exam.kata;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class SpringBootWebApplicationTest {
+
+	@Test
+	public void testSpringBootSetUp() {
+	    assertTrue(true);
+   	}   
+	
+}
