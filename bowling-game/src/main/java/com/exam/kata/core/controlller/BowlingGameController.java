206adeea6b8deceb61953836b3bfb8000d3f4c79
2019-Q2_SOW_DEV_033 <2019-Q2_SOW_DEV_033@gmail.com>
2019-07-10 12:30:08 +0200

    Created Rest Spring Controller Test case and exposed the service /bowling-game/score

---
+++ bowling-game/src/main/java/com/exam/kata/core/controlller/BowlingGameController.java
@@ -0,0 +1,14 @@
+package com.exam.kata.core.controlller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BowlingGameController
+{
+	@RequestMapping(value="/bowling-game/score", method = RequestMethod.GET)
+    public @ResponseBody void getScore() {
+    }
+}
---
+++ bowling-game/src/test/java/com/exam/kata/core/controlller/BowlingGameControllerTest.java
@@ -0,0 +1,36 @@
+package com.exam.kata.core.controlller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public class BowlingGameControllerTest
+{
+ 
+    @InjectMocks
+    private BowlingGameController bowlGameController;
+ 
+    private MockMvc mockMvc;
+ 
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(bowlGameController).build();
+    }
+    
+    @Test
+    public void testCreateSignupFormInvalidUser() throws Exception {
+        this.mockMvc.perform(get("/bowling-game/score"))
+                    .andExpect(status().isOk());
+    }
+}
